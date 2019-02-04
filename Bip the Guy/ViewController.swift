7cf5f80644727ce7d2fc85699b6610867388aad6
James Steele <jwsteele1@me.com>
2019-02-04 14:12:56 -0500

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by James Steele on 2/4/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
