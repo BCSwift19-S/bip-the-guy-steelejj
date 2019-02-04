4a78c8a103c00b5784dea5050cdb70475b92bfeb
James Steele <jwsteele1@me.com>
2019-02-04 14:28:54 -0500

    4.2

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+// actions
+    
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds = self.imageToPunch.bounds
+        
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + 60, y: self.imageToPunch.bounds.origin.y + 60, width: self.imageToPunch.bounds.size.width - 60, height:self.imageToPunch.bounds.size.height - 60)
+    }
+    
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+// whats wrong here? why cant i add as function?
+    @IBOutlet weak var imageTapped: UIImageView!
+    
 
 }
 
