366844a402cacbd2e22bb9189cd4a02b1e04c8c3
steelejj <43275285+steelejj@users.noreply.github.com>
2019-02-11 15:11:52 -0500

    Add files via upload

---
+++ Bip the Guy steelejj/Bip the Guy/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by James Steele on 2/10/19.
+//  Copyright © 2019 James Steele. All rights reserved.
+
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageToPunch: UIImageView!
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // Mark: Functions
+    
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        // shrink out imageToPunch by 60 pixels
+        self.imageToPunch.bounds = CGRect(
+            x: self.imageToPunch.bounds.origin.x + shrinkValue,
+            y: self.imageToPunch.bounds.origin.y + shrinkValue,
+            width: self.imageToPunch.bounds.size.width - shrinkValue,
+            height: self.imageToPunch.bounds.size.height - shrinkValue
+        )
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations : {self.imageToPunch.bounds = bounds}, completion: nil
+        )
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        
+        if let sound = NSDataAsset(name: soundName) {
+            // chgeck if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("error, /(soundname) could'nt be played")
+            }
+        } else {
+            print("Error, /(soundname) doesn't exist")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let selectedImage =
+            info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+
+        imageToPunch.image = selectedImage
+
+        dismiss(animated: true, completion: nil)
+    }
+        
+    func showAlert(title: String, message: String) {
+        let alertCountroller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertCountroller.addAction(defultAction)
+        present(alertCountroller, animated: true, completion: nil)
+        }
+    
+    
+    // Mark: Actions
+    
+    @IBAction func libraryPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    @IBAction func cameraPressed(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            imagePicker.delegate = self
+        } else {
+            showAlert(title: "Camera Not Available", message: "There is no camera available on this device")
+        }
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+        print("pressed")
+    }
+    
+}
+
